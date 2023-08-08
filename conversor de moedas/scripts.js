5d9e33ab9f5af31183ff226151e22a80cca55c00
Adriano Luiz dos Anjos <128822541+Adrianodosanjoos@users.noreply.github.com>
2023-08-07 22:13:21 -0300

    Add files via upload

---
+++ conversor de moedas/scripts.js
@@ -0,0 +1,65 @@
+
+const Button = document.getElementById("convert-button");
+const Select = document.getElementById("currency-select");
+
+const convertValues = async () => {
+    const inputReais = document.getElementById("input-currency").value;
+    const realValueText = document.getElementById("currency-value-to-convert");
+    const currencyValueText = document.getElementById("currency-value");
+
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+
+    realValueText.innerHTML = new Intl.NumberFormat(pt-BR, {
+        style:"currency",
+        currency: "BRL",
+    }).format(inputReais);
+
+
+
+    if (currencySelect.value == "dolar") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+}
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src = "./assets/estados-unidos (1) 1.png"
+    }
+
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
+
+    convertValues()
+
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
